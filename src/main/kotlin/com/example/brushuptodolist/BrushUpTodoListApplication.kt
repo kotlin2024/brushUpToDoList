316596f937432fe587bd107881812a6db4a785b2
PHJUNITY <45454119+PHJUNITY@users.noreply.github.com>
2024-06-25 14:38:52 +0900

    :tada: Begin Project

---
+++ src/main/kotlin/com/example/brushuptodolist/BrushUpTodoListApplication.kt
@@ -0,0 +1,11 @@
+package com.example.brushuptodolist
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BrushUpTodoListApplication
+
+fun main(args: Array<String>) {
+    runApplication<BrushUpTodoListApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/brushuptodolist/BrushUpTodoListApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.brushuptodolist
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BrushUpTodoListApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
